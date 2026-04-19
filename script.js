6966aae8c63e643ceb36bb7de0945d50ae178a2f
pappu993984 <pappukumar993984@gmail.com>
2026-04-20 00:57:17 +0530

    updated website content

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("😊 Welcome to Pikku's First website. 😊")
+function toggleDark() {
+  document.body.classList.toggle("dark");
+}
+
+function closePopup() {
+  document.getElementById("popup").classList.remove("show");
+  document.body.classList.remove("blur");
+}
+
+window.onload = function() {
+  setTimeout(() => {
+    document.getElementById("popup").classList.add("show");
+    document.body.classList.add("blur");
+  }, 800);
+};
