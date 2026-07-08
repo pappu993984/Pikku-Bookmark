d331241f7730fc85c0fc2323b870f6ab3beca1b2
pappu993984 <pappukumar993984@gmail.com>
2026-07-08 22:45:00 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function toggleDark() {
   document.body.classList.toggle("dark");
 }
@@ -17,24 +16,4 @@ window.onload = function() {
 
     document.body.style.overflow = "hidden"; // scroll OFF
   }, 800);
-=======
-function toggleDark() {
-  document.body.classList.toggle("dark");
-}
-
-function closePopup() {
-  document.getElementById("popup").classList.remove("show");
-  document.getElementById("blurBg").classList.remove("show");
-
-  document.body.style.overflow = "auto"; // scroll ON
-}
-
-window.onload = function() {
-  setTimeout(() => {
-    document.getElementById("popup").classList.add("show");
-    document.getElementById("blurBg").classList.add("show");
-
-    document.body.style.overflow = "hidden"; // scroll OFF
-  }, 800);
->>>>>>> 27419c8fa4a13a4156ff50b0beb208c1a7b6192b
 };
